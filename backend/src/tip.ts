292d28964c839b53d891f1e0dc295286a0cb93c6
BradenBagby <bradenjbagby@gmail.com>
2025-01-30 12:12:44 -0700

    tip message

--- backend/src/tip.ts
+++ backend/src/tip.ts
@@ -31,7 +31,7 @@ export const tipCommand = async ({ client, body, command }: SlackCommandMiddlewa
                 type: "section",
                 text: {
                     type: "mrkdwn",
-                    text: "Select an amount to generate a QR code:"
+                    text: "Select an amount to tip:"
                 }
             },
             {
